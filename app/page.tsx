3875b54bfd092ef0d24408971ea99fd18dde7ce4
Tarik Raposo <tarik.parreira@mpdft.mp.br>
2026-03-20 15:39:51 -0300

    Adiciona componentes para criar na página inicial.

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,27 @@
-import Sections from "./components/Sections";
+"use client"
 
-export default function Home() {
+import { SmoothScroll } from '@/components/smooth-scroll'
+import { Header } from '@/components/header'
+import { Scene3D } from '@/components/scene-3d'
+import { HeroSection } from '@/components/sections/hero-section'
+import { AboutSection } from '@/components/sections/about-section'
+import { HowItWorksSection } from '@/components/sections/how-it-works-section'
+import { ContactSection } from '@/components/sections/contact-section'
+
+export default function HomePage() {
   return (
-    <main className="main">
-      <div className="bg" />
-
-      <Sections />
-    </main>
-  );
+    <SmoothScroll>
+      <main className="relative">
+        <Header />
+        <Scene3D />
+        
+        <div className="relative z-20">
+          <HeroSection />
+          <AboutSection />
+          <HowItWorksSection />
+          <ContactSection />
+        </div>
+      </main>
+    </SmoothScroll>
+  )
 }
